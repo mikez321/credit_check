53056877c60057758001c99258440cfb28421481
Mike Hernandez <hernandezmichael321@gmail.com>
2020-01-22 10:50:53 -0700

    Condense code

--- lib/credit_check.rb
+++ lib/credit_check.rb
@@ -2,18 +2,15 @@ card_number = "5541808923795240"
 
 # Your Luhn Algorithm Here
 
-a = card_number.split("").collect{|i| i.to_i }
+a = card_number.split("").collect{|digits| digits.to_i }
 
-a_evens = a.select.with_index{ |value, index| index % 2 == 0}.collect{ |n| n * 2}
-a_odds = a.select.with_index{ |value, index| index % 2 == 1}
+a_evens = a.select.with_index{ |value, index| index.even? }.collect{ |n| n * 2}
+a_odds = a.select.with_index{ |value, index| index.odd?}
 
 non_splits = a_evens.select { |n| n < 10}
-splits = a_evens.select { |n| n >= 10 }
+splits = a_evens.select { |n| n >= 10 }.join("").split("").collect { |n| n.to_i}
 
-lhun1 = splits.join("").split("")
-lhun2 = lhun1.collect { |n| n.to_i }
-
-final = a_odds.reduce(:+) + non_splits.reduce(:+) + lhun2.reduce(:+)
+final = a_odds.reduce(:+) + non_splits.reduce(:+) + splits.reduce(:+)
 
 if final % 10 == 0
   puts "The number #{card_number} is valid!"
